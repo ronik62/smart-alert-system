5266b1d1c25d1644529b20eb70e81e3355271f1c
ronik62 <ronikkumbhar62@gmail.com>
2026-04-02 14:20:55 +0530

    fix

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = "https://smart-alert-system.onrender.com";
+const BASE_URL = "https://smart-alert-system.onrender.com/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
